ec58b18f1b68bf803e1f9640fb85f236362ef475
wolfe-colton <32718267+wolfe-colton@users.noreply.github.com>
2018-04-04 10:10:59 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+		
+	$serverName = "localhost";   
+	$uid = "co297720";     
+	$pwd = "3KblNsL0azpFkGj8";    
+	$databaseName = "co297720";     
+	    
+	// Connect using mySQL Authentication.    
+	$conn = mysqli_connect( $serverName, $uid, $pwd, $databaseName);
+
+
+?>
